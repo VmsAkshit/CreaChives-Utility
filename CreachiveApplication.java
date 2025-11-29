09fd3ca16ff7d5378de897ae6bf5ef3dbb4c26f5
Akshit <am5859241@gmail.com>
2025-11-29 21:00:28 +0530

    Update CreachiveApplication.java

--- CreachiveApplication.java
+++ CreachiveApplication.java
@@ -2,23 +2,10 @@ package com.creachive.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Main Entry point for the Creachive Backend.
- * To run this, you need Maven/Gradle and a standard Spring Boot Project structure.
- */
 @SpringBootApplication
-@RestController
 public class CreachiveApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CreachiveApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String healthCheck() {
-        return "Creachive Backend is Running! version 1.0.0";
     }
 }
